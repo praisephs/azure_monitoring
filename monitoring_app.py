49ee0831962cde0347ca6fe9be02bf81fe5045e3
Uyi Oboite <uyioboite@gmail.com>
2024-09-18 16:55:34 +0100

    Updated the monitoring_app file

--- monitoring_app.py
+++ monitoring_app.py
@@ -39,6 +39,7 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 import psutil
 from fastapi.responses import HTMLResponse
+from pathlib import Path
 
 app = FastAPI()
 
